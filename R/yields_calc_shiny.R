86ff724abdfc71c9cad15f0ed161168732f79ff3
JanStraehl <73852480+JanStraehl@users.noreply.github.com>
2021-03-23 13:19:29 +0100

    additional output values
    charring total, filter area and EC-Yield for single steps

--- R/yields_calc_shiny.R
+++ R/yields_calc_shiny.R
@@ -46,24 +46,26 @@ filename <<- input$fileUploadedOC$datapath
 df_raw <- NULL
 for (i in filename){
   data_load_func(i)
-  df_raw <- rbind(df_raw, data.frame(tabla_resultados2$EC_yield,tabla_resultados2$charringS1,tabla_resultados2$charringS2,tabla_resultados2$charringS3))
+  input_data <- read.csv(file = i, sep = ",", col.names = paste0("C",seq_len(20)), fill = TRUE, header = T)
+  filter_area = as.numeric(input_data[min(grep('FilterArea',input_data$C1))+1,1])
+  df_raw <- rbind(df_raw, data.frame(tabla_resultados2$EC_yield_S1,tabla_resultados2$EC_yield_S2,tabla_resultados2$EC_yield_S3,tabla_resultados2$charring_S1,tabla_resultados2$charring_S2,tabla_resultados2$charring_S3,tabla_resultados2$charring_total,filter_area))
 }
 
 #rm(list=setdiff(ls(), c("df","result_filename", "csv_raw", "csv_stat", "csv_mean")))
-#df_raw$filter <- c(rep(result_filename, length(df_raw$tabla_resultados2.EC_yield)))
+#df_raw$filter <- c(rep(result_filename, length(df_raw$tabla_resultados2.EC_yield_S1)))
 df_raw$filter <- result_filename
-colnames(df_raw) <- c("EC_yield", "charring_S1", "charring_S2", "charring_S3", "filter_name")
+colnames(df_raw) <- c("EC_yield_S1", "EC_yield_S2", "EC_yield_S3", "charring_S1", "charring_S2", "charring_S3", "charring_total", "filter_area_cm2", "filter_name")
 
 #extract specific data--------------------------------------------------------------
-df_length_raw <- length(df_raw$EC_yield)
-df_charr_raw <- data.frame("charring value" = c(df_raw$charring_S1,df_raw$charring_S2,df_raw$charring_S3), "Sunset step" = c(rep("S1",df_length_raw), rep("S2",df_length_raw), rep("S3",df_length_raw)), stringsAsFactors = FALSE)
-df_yield_raw <- data.frame("EC-yield" = df_raw$EC_yield, "dummy"= rep("",df_length_raw), stringsAsFactors = FALSE)
+df_length_raw <- length(df_raw$EC_yield_S1)
+df_charr_raw <- data.frame("charring value" = c(df_raw$charring_S1,df_raw$charring_S2,df_raw$charring_S3,df_raw$charring_total), "Sunset step" = c(rep("S1",df_length_raw), rep("S2",df_length_raw), rep("S3",df_length_raw), rep("total",df_length_raw)), stringsAsFactors = FALSE)
+df_yield_raw <- data.frame("EC-yield" = c(df_raw$EC_yield_S1,df_raw$EC_yield_S2,df_raw$EC_yield_S3), "Sunset step"= c(rep("S1",df_length_raw), rep("S2",df_length_raw), rep("S3",df_length_raw)), stringsAsFactors = FALSE)
 
 #plots with raw data----------------------------------------------------------------
 
 #plot: EC-yield (raw data)
 theme_set(theme_classic(base_size = 13))
-plot_yield_raw <- ggboxplot(width=0.33, data=df_yield_raw, x = "dummy", y = "EC.yield",  xlab="", ylab = "EC-yield")
+plot_yield_raw <- ggboxplot(width=0.33, data=df_yield_raw, x = "Sunset.step", y = "EC.yield",  xlab="Sunset step", ylab = "EC-yield")
 #plot: charring for each Sunset step (raw data)
 plot_charr_raw <- ggboxplot(data=df_charr_raw, x="Sunset.step", y="charring.value", xlab="Sunset step", ylab = "charring")+
   geom_hline(yintercept = 0, color="red", lty= 5)
@@ -73,7 +75,7 @@ fig_EC_yield_charring_raw <- fig_EC_yield_charring_raw + theme(plot.margin = mar
 fig_EC_yield_charring_raw <- annotate_figure(fig_EC_yield_charring_raw, top = text_grob("Raw data", color = "darkred", face = "bold", size = 12))
 
 #remove outliers-------------------------------------------------------------------
-iqr_scores <- abs(scores(df_raw[,1:4],type="iqr"))
+iqr_scores <- abs(scores(df_raw[,3:6],type="iqr"))
 outlier_rows <- unique(which((iqr_scores>IQR) == TRUE, arr.ind=TRUE)[,1])
 n_outlier_rows <- length(outlier_rows)
 if (n_outlier_rows == 0) {
@@ -83,14 +85,14 @@ if (n_outlier_rows == 0) {
 }
 
 #extract specific data w/o outliers-------------------------------------------------
-df_length <- length(df$EC_yield)
-df_charr <- data.frame("charring value" = c(df$charring_S1,df$charring_S2,df$charring_S3), "Sunset step" = c(rep("S1",df_length), rep("S2",df_length), rep("S3",df_length)), stringsAsFactors = FALSE)
-df_yield <- data.frame("EC-yield" = df$EC_yield, "dummy"= rep("",df_length), stringsAsFactors = FALSE)
+df_length <- length(df$EC_yield_S3)
+df_charr <- data.frame("charring value" = c(df$charring_S1,df$charring_S2,df$charring_S3,df$charring_total), "Sunset step" = c(rep("S1",df_length), rep("S2",df_length), rep("S3",df_length), rep("total",df_length)), stringsAsFactors = FALSE)
+df_yield <- data.frame("EC-yield" = c(df$EC_yield_S1,df$EC_yield_S2,df$EC_yield_S3), "Sunset step"= c(rep("S1",df_length), rep("S2",df_length), rep("S3",df_length)), stringsAsFactors = FALSE)
 
 #calculate stats and generate plots-------------------------------------------------
 
 #general stats
-stat <- stat.desc(df[, -5])
+stat <- stat.desc(df[, -9])
 #mean of each EC-yield and each Sunset step
 df_mean <- stat[9,]
 
@@ -100,7 +102,7 @@ df_mean <- stat[9,]
 
 #plot: EC-yield
 theme_set(theme_classic(base_size = 13))
-plot_yield <- ggboxplot(width=0.33, data=df_yield, x = "dummy", y = "EC.yield",  xlab="", ylab = "EC-yield", outlier.shape=NA)+
+plot_yield <- ggboxplot(width=0.33, data=df_yield, x = "Sunset.step", y = "EC.yield",  xlab="Sunset step", ylab = "EC-yield", outlier.shape=NA)+
   geom_jitter(width=0.1,height=0, colour = "#424242")
 #plot: charring for each Sunset step
 plot_charr <- ggboxplot(data=df_charr, x="Sunset.step", y="charring.value", xlab="Sunset step", ylab = "charring", outlier.shape=NA)+
@@ -112,8 +114,9 @@ fig_EC_yield_charring_wo_outliers <- fig_EC_yield_charring_wo_outliers + theme(p
 fig_EC_yield_charring_wo_outliers <- annotate_figure(fig_EC_yield_charring_wo_outliers, top = text_grob("Outliers removed", color = "darkblue", face = "bold", size = 12))
 
 #stats table
-colnames(stat) <- c("EC-yield", "charring S1", "charring S2", "charring S3")
-stat_table <- ggtexttable(format(stat[c(9,4:6,8,12,13),], digits = 3),  theme = ttheme("blank"))
+colnames(stat) <- c("EC-yield S1", "EC-yield S2", "EC-yield S3", "charring S1", "charring S2", "charring S3", "charring total", "filter_area_cm2")
+stat_table <- ggtexttable(format(stat[c(9,4:6,8,12,13),c(1:7)], digits = 3),  theme = ttheme("blank", base_size=10))
+colnames(stat) <- c("EC_yield_S1", "EC_yield_S2", "EC_yield_S3", "charring_S1", "charring_S2", "charring_S3", "charring_total", "filter_area_cm2") #rename columns for csv-output
 
 #generate summary pdfs---------------------------------------------------------------
 
@@ -126,8 +129,8 @@ ggsave(filename = tempPdf1, plot = fig_summary1_temp , width = 8.3, height = 11.
 #summary QQ-Plots-------------------------------------------------------------------
 
 #QQ-plots EC
-plot_qq_EC_raw <- ggqqplot(df_raw$EC_yield, ylab = "EC-yield", title = "Raw data")
-plot_qq_EC_corr <- ggqqplot(df$EC_yield, ylab = "EC-yield", title = "Outliers removed")
+plot_qq_EC_raw <- ggqqplot(df_raw$EC_yield_S3, ylab = "EC-yield total", title = "Raw data")
+plot_qq_EC_corr <- ggqqplot(df$EC_yield_S3, ylab = "EC-yield total", title = "Outliers removed")
 #QQ-plots charring S1
 plot_qq_S1_raw <- ggqqplot(df_raw$charring_S1, ylab = "Charring S1")
 plot_qq_S1_corr <- ggqqplot(df$charring_S1, ylab = "Charring S1")
